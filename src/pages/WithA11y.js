dc592a9dcd22240215d611ec1400937b995ba21a
Johanna <nijodotcom@gmail.com>
2021-01-23 12:42:29 +0100

    dark mode and media query prefers-color-scheme = respect users settings

--- src/pages/WithA11y.js
+++ src/pages/WithA11y.js
@@ -1,12 +1,43 @@
 import NavigationWithA11y from "../components/NavigationWithA11y";
 
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+
+const DARK_CLASS = "dark";
+
 const WithA11y = () => {
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme: dark)",
+    },
+    undefined,
+    (prefersDark) => {
+      setIsDark(prefersDark);
+    }
+  );
+  const [isDark, setIsDark] = useState(systemPrefersDark);
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add(DARK_CLASS);
+    } else {
+      document.documentElement.classList.remove(DARK_CLASS);
+    }
+  }, [isDark]);
+
+  console.log("is dark", isDark);
   return (
     <main>
       <header>
         <h1>With A11Y</h1>
         <NavigationWithA11y />
       </header>
+      <input
+        type="checkbox"
+        checked={isDark}
+        value={isDark}
+        onChange={(e) => setIsDark(e.target.checked)}
+        aria-label="Dark mode"
+      />
 
       <p>Testilitest</p>
     </main>
